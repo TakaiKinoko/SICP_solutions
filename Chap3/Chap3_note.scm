07711b1f5307d44efc0545b7ec86121bf06e1de1
Fang Han <hanfangviolin@gmail.com>
2018-12-05 23:50:42 -0500

    day 2

--- Chap3/Chap3_note.scm
+++ Chap3/Chap3_note.scm
@@ -1,5 +1,6 @@
 #lang scheme
 
+;-------------------- VERSION 1 --------------
 ; state variable
 (define balance 100)
 
@@ -16,6 +17,7 @@
 ; <expk> will be returned as the value of the whole sequence. (same as OCaml sequence)
 
 
+;-------------------- VERSION 2 ---------------
 (define withdraw-safe   ; with balance being local to withdraw, any other procedures will not be able to access balance unless through here 
   (let ((balance 100))  ; use let to establish an environment with balance encapsulated within
     (lambda (amount)
@@ -23,3 +25,16 @@
           (begin (set! balance (- balance amount))
                  balance)
           "Insufficient funds"))))
+
+
+;-------------------- VERSION 3 ----------------
+;; creating independent objects each with its own local state variable
+(define (make-withdraw balance)
+  (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+(define W2 (make-withdraw 100))
