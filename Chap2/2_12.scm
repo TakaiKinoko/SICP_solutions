c3ec33621111126fcdd84dc5b43001cacb43a23b
Fang Han <hanfangviolin@gmail.com>
2018-12-27 14:03:19 -0500

    12/27

---
+++ Chap2/2_12.scm
@@ -0,0 +1,14 @@
+#lang scheme
+(require racket/include)
+(require "2_7.scm")
+
+; percentage tolerance is defined as the width of the interval
+; divided by the midpoint of the interval
+
+(define (make-center-percent c p)
+  (let ((half (/ (* c p) 2)))
+    (make-interval (- c half) (+ c half))))
+
+;tests
+(make-center-percent 100 0.5)
+(make-center-percent 1 0.2)
