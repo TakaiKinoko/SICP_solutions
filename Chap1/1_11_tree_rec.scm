3b5b9628b13a5553c6d5951d3ad63b14c036e38d
Fang Han <hanfangviolin@gmail.com>
2019-01-09 15:31:05 +0800

    initial commit

--- Chap1/1_11_tree_rec.scm
+++ Chap1/1_11_tree_rec.scm
@@ -1,2 +1,34 @@
 #lang scheme
 
+; recursive
+
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1)) (f (- n 2)) (f (- n 3))))))
+
+; tests
+(f 0)
+(f 1)
+(f 2)
+(f 3)
+(f 4)
+
+; iterative
+(define (f-iter n)
+  (define (aux x y z counter)
+    (cond ((< n 3) n)
+          ((= counter n) (+ x y z))
+          (else (aux y z (+ x y z) (+ counter 1)))))
+  (aux 0 1 2 3))
+
+; tests
+(newline)
+(f-iter 0)
+(f-iter 1)
+(f-iter 2)
+(f-iter 3)
+(f-iter 4)
+(f-iter 100)
+
+; should be suuuuper slow
+(f 100)
